e491bd8cc46bed86ca338b9924e7d2ef26f0bb73
pooja28ydv <ydvpooja2812@gmail.com>
2025-07-03 16:07:27 +0530

    Add Initial Files

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 
 const store = MongoStore.create({
-      mongoUrl : dbUrl,
+      mongoUrl : process.env.ATLASDB_URL,
       crypto: {
            secret: process.env.SECRET,
       },
