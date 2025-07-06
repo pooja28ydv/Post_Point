69a631ab53938b792372b5edaef3e8509b417668
pooja28ydv <ydvpooja2812@gmail.com>
2025-07-06 13:59:59 +0530

    Add Initial Files

--- controllers/listings.js
+++ controllers/listings.js
@@ -81,7 +81,7 @@ module.exports.createListing = async (req, res) => {
     //   newListing.category = req.body.listing.category;
       await newListing.save();
       req.flash("success", "New Blog Created");
-      return res.redirect("/listings");
+       res.redirect("https://post-point.onrender.com/listings");
     
 
     } catch (error) {
